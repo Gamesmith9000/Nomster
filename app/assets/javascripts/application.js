db3cf85eda32f452ba4959a8e6de3b8b99a4a968
Julian Smith <smith.julian.f@gmail.com>
2019-10-10 17:23:14 +0000

    Set project up for Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
+
 //= require_tree .
+
+// [JFS] Remember that bootstrap-sprockets needs to be after turbolinks (noted by instructor)
