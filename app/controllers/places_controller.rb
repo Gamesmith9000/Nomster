3e11e464966c981d161ec79cc8b4ba2393bdc616
Julian Smith <smith.julian.f@gmail.com>
2019-10-19 19:14:29 +0000

    Modified user permissions for 'Edit'

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all
@@ -20,10 +20,21 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    # Reminder: without the return above, the code would keep running
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
