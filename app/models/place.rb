30999477758f99a6982233ccda22870cef5a9bc7
Julian Smith <smith.julian.f@gmail.com>
2019-10-23 17:39:19 +0000

    Added map to 'place' page

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
