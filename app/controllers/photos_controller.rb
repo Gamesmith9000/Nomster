f47c669cc85b8460a8425a4633592e4878d8de6d
Julian Smith <smith.julian.f@gmail.com>
2019-11-06 22:03:43 +0000

    Final touches

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,10 +3,10 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-  #  @place.photos.create(photo_params)
-#above line was :     
-@place.photos.create(photo_params.merge(user: current_user))
-# but could not resolve error    
+    #  @place.photos.create(photo_params)
+    #  Line above was the original version of next line     
+    @place.photos.create(photo_params.merge(user: current_user))
+    # but could not resolve error    
     redirect_to place_path(@place)
   end
 
