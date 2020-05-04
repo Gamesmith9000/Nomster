9241936c5d8355ece1724eb9b5209e63b528f82b
Julian Smith <smith.julian.f@gmail.com>
2020-05-04 12:47:04 -0500

    Adjusted some spacing and formatting for models and master stylesheet

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,6 +9,4 @@ class Place < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
-
-  # IMPORTANT: The built-in Rails validations take place at the MODEL level
 end
